c1f7f38bc9e56bd9fea5a04423bc8ad59e128ee7
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-02-16 20:29:19 +0600

    ChatHub for Hub implementation

---
+++ BlazorServer/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+namespace BlazorServer.Hubs
+{
+	public class ChatHub : Hub
+	{
+		/// <summary>
+		/// to send message to every clients
+		/// </summary>
+		/// <param name="user">the user to send</param>
+		/// <param name="message">the message to send</param>
+		/// <returns></returns>
+		public Task SendMessage(string user, string message)
+		{
+			return Clients.All.SendAsync("ReceiveMessage",user, message);
+		}
+	}
+}
+
